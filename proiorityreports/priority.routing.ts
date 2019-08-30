46218999fe087cf72c9b2fd92f714488e3b85f55
mani9100 <45023389+mani9100@users.noreply.github.com>
2019-08-30 16:09:49 +0530

    select and deselect all in options in angular
    select and deselect all in options in angular

---
+++ proiorityreports/priority.routing.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { PriorityReportManageComponent } from './manage/manage.component';
+
+
+export const PriorityReportRoutes: Routes = [
+    {
+        path: '',
+        component: PriorityReportManageComponent
+    }
+]
---
+++ proiorityreports/priorityreport.module.ts
@@ -0,0 +1,45 @@
+import { CommonModule, DatePipe } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { TruncateModule } from 'ng2-truncate';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { FileHelpersModule } from 'ngx-file-helpers';
+import { FileUploadModule } from 'ng2-file-upload';
+import { AmazingTimePickerModule } from 'amazing-time-picker';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { sharedDirectiveModule } from '../../_directives/sharedDirectives';
+import { MaterialModule } from '../../_shared/material.module';
+import { PriorityReportRoutes } from './priority.routing';
+import { PriorityReportManageComponent } from './manage/manage.component';
+import { DetailedPriorityReportsComponent } from './detailed-priority-reports/detailed-priority-reports.component';
+
+ 
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forChild(PriorityReportRoutes),
+        sharedDirectiveModule,
+        MaterialModule,
+       TranslateModule,
+       TruncateModule,
+       FlexLayoutModule,
+       PerfectScrollbarModule,
+       FileHelpersModule,
+       FileUploadModule,
+       AmazingTimePickerModule,
+       NgxMaterialTimepickerModule
+ ],
+ declarations: [
+    PriorityReportManageComponent,
+    DetailedPriorityReportsComponent
+  ],
+
+  entryComponents: [],
+  providers: [DatePipe]
+    })
+export class PriorityReportModule { }
